8175a26a9068a7c37652e487b6b8b1f5a5b991b5
Peter Fitzpatrick <peterfitzpatrick@Peters-MacBook-Pro.local>
2019-03-01 15:25:44 -0500

    created seeds.js

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -2,7 +2,7 @@ const Link = require('../models/Link.js')
 
 const linkController = {
     index: (req, res) => {
-        res.send('hi from index')
+        res.render('index')
     },
     new: (req, res) => {
         res.send('Hi from the new view')
---
+++ db/seeds.js
@@ -0,0 +1,52 @@
+const mongoose = require("./connection.js");
+const Link = require("../models/Link.js");
+
+// const fellinis = new Shop({
+//     name: "Fellini's Pizza",
+//     rating: 4,
+//     category: "Pizza",
+//     description:
+//       "Fellini's Pizza is an Italian restaurant founded in Atlanta on May 5, 1982 by Clay Harper of the Coolies. It has 7 locations and is known for its Atlanta style pizza. Its first location was opened near Little Five Points. It was voted the best pizza restaurant in Atlanta in 2013 by the Emory Wheel",
+//     imgLink:
+//       "https://thepizzasnob.files.wordpress.com/2012/08/putdooes-resize.jpg",
+//     foodItems: [pizza]
+//   });
+
+const firstOne = new Link({
+    url: 'https://thepizzasnob.files.wordpress.com/2012/08/putdooes-resize.jpg',
+    title: 'fist post',
+    desc: 'Our very first post',
+    date: Date.now()
+})
+const secondOne = new Link({
+    url: 'https://thepizzasnob.files.wordpress.com/2012/08/putdooes-resize.jpg',
+    title: 'second post',
+    desc: '2',
+    date: Date.now()
+})
+const thirdOne = new Link({
+    url: 'https://thepizzasnob.files.wordpress.com/2012/08/putdooes-resize.jpg',
+    title: 'third post',
+    desc: '3',
+    date: Date.now()
+})
+const fourthOne = new Link({
+    url: 'https://thepizzasnob.files.wordpress.com/2012/08/putdooes-resize.jpg',
+    title: 'fourth post',
+    desc: '4',
+    date: Date.now()
+})
+const fifthOne = new Link({
+    url: 'https://thepizzasnob.files.wordpress.com/2012/08/putdooes-resize.jpg',
+    title: 'fif post',
+    desc: '5',
+    date: Date.now()
+})
+
+Link.remove({})
+  .then(() => Link.create([firstOne, secondOne, thirdOne, fourthOne, fifthOne]))
+  .then(() => {
+    console.log("seeded successfully");
+    mongoose.connection.close();
+  })
+  .catch(err => console.log(err, "error!"));
